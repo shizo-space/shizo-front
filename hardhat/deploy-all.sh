c19b42f7229a843f10d1ce641408f4b30f48314f
MohammadReza Lotfi <mohamadmlnnamin@gmail.com>
2022-07-06 16:48:37 +0430

    Fix scripts

--- hardhat/deploy-all.sh
+++ hardhat/deploy-all.sh
@@ -2,20 +2,15 @@
 
 export HOST=$(hostname)
 
-yarn chain --hostname 0.0.0.0 &
-sleep 1
 export CONTRACT=$(npx hardhat deploy --export-all ../src/contracts/hardhat_contracts.json --network localhost --gasprice 10000000000000 --reset | grep Chest | cut -d ' ' -f 7)
 
-npx hardhat request-random-words --contract $CONTRACT --network localhost &
-npx hardhat request-random-words --contract $CONTRACT --network localhost &
-npx hardhat request-random-words --contract $CONTRACT --network localhost &
+npx hardhat request-random-words --contract $CONTRACT --network localhost
+npx hardhat request-random-words --contract $CONTRACT --network localhost
+npx hardhat request-random-words --contract $CONTRACT --network localhost
 
 cd subgraphs
+envsubst < subgraph-template.yaml > subgraph.yaml
 docker-compose up -d
 sleep 5 # wait
-yarn create-local &
-yarn deploy-local &
-
-wait -n
-
-exit $?
+yarn create-local
+yarn deploy-local
